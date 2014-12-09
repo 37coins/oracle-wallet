42f0da40bd3f14df796d86392e6f360caf87916c
Jonathan Brown <jbrown@bluedroplet.com>
2014-12-09 15:54:25 +0700

    Updates.

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ $(function() {
   var Address = bitcore.Address;
   var child;
   var uuid;
-  
+
   function display_account() {
-  
     var unconfirmed_balance;
     $.ajax({
       type: "GET",
@@ -56,7 +55,6 @@ $(function() {
       var row = "<tr><td>" + transaction.time_utc + "</td><td>" + transaction.netAmount + "</td></tr>";
       console.log(row);
       
-      
       $("table#transactions tbody").append(row);
     }
     
@@ -72,7 +70,6 @@ $(function() {
       }
     });
 
-
     console.log(address);
 
     var qrcode = new QRCode($("#address")[0], {
@@ -80,120 +77,135 @@ $(function() {
       width: 128,
       height: 128,
     });
+
+    $("#loader").hide();
+    $("#account").show();
   }
 
-$("#new-account").click(function() {
+$("#new-account").submit(function(event) {
+  event.preventDefault();
+  $("#account-forms").fadeOut('fast', function () {
+    $("#loader").show();
+    
+    
+    var phone = $("#phone").val();
+    console.log(phone);
 
-  var BIP39WordlistEn = bitcore.BIP39WordlistEn;
+    var BIP39WordlistEn = bitcore.BIP39WordlistEn;
+    var mnemonic = BIP39.mnemonic(BIP39WordlistEn, 256);
+    var seed = BIP39.mnemonic2seed(mnemonic);
+    var hkey = HierarchicalKey.seed(seed);
+    child = hkey.derive("m/0'");
 
-  var mnemonic = BIP39.mnemonic(BIP39WordlistEn, 256);
-
-  console.log(mnemonic);
-  var seed = BIP39.mnemonic2seed(mnemonic);
-
-
-  var hkey = HierarchicalKey.seed(seed);
-  child = hkey.derive("m/0'");
-
-  var xpub = child.extendedPublicKeyString();
+    var xpub = child.extendedPublicKeyString();
 
 
-  console.log(xpub);
+    console.log(xpub);
 
 
-  uuid = UUID.v5(xpub, "urn:37coins.com");
+    uuid = UUID.v5(xpub, "urn:37coins.com");
 
-  console.log(uuid);
+    console.log(uuid);
 
-  var account = {
-      "phone": "+66967605420",
-      limit: 1000000,
-      "xpubs": [
-          xpub
+    var account = {
+        "phone": phone,
+        limit: 1000000,
+        "xpubs": [
+            xpub
+        ],
+        "locale" : "en"
+    }
+
+    var oracle_xpub;
+
+    $.ajax({
+      type: "POST",
+      url: "http://oracle-qa.37coins.io/api/account/" + uuid,
+      async: false,
+      data: JSON.stringify(account),
+      success: function(data) {
+        oracle_xpub = data.keychain;
+      },
+      contentType: 'application/json',
+      dataType: 'json'
+    });
+
+    console.log(oracle_xpub);
+
+    var account = {
+      "walletAgent": "",
+      "keys": [
+        xpub,
+        oracle_xpub
       ],
-      "locale" : "en"
-  }
+      "requiredSigCount" : 2
+    }
 
-  var oracle_xpub;
+    $.ajax({
+      type: "POST",
+      url: "http://chains-qa.37coins.io/keychains/" + uuid,
+      async: false,
+      data: JSON.stringify(account),
+      success: function(data, textStatus, jqXHR) {
+        console_log(textStatus);
+      },
+      contentType: 'application/json',
+      dataType: 'json'
+    });
 
-  $.ajax({
-    type: "POST",
-    url: "http://oracle-qa.37coins.io/api/account/" + uuid,
-    async: false,
-    data: JSON.stringify(account),
-    success: function(data) {
-      oracle_xpub = data.keychain;
-    },
-    contentType: 'application/json',
-    dataType: 'json'
+  /*
+    $.ajax({
+      type: "POST",
+      url: "http://oracle-qa.37coins.io/api/account/" + uuid + "/pin",
+      async: false,
+      contentType: 'application/json',
+      dataType: 'json'
+    });
+  */
+
+    alert("Account created. Write down the following mnemonic sentence and keep it safe. It is the only way to access your account: " + mnemonic);
+
+    window.location.reload(true);
   });
-
-  console.log(oracle_xpub);
-
-  var account = {
-    "walletAgent": "",
-    "keys": [
-      xpub,
-      oracle_xpub
-    ],
-    "requiredSigCount" : 2
-  }
-
-  $.ajax({
-    type: "POST",
-    url: "http://chains-qa.37coins.io/keychains/" + uuid,
-    async: false,
-    data: JSON.stringify(account),
-    success: function(data, textStatus, jqXHR) {
-      console_log(textStatus);
-    },
-    contentType: 'application/json',
-    dataType: 'json'
-  });
-
-/*
-  $.ajax({
-    type: "POST",
-    url: "http://oracle-qa.37coins.io/api/account/" + uuid + "/pin",
-    async: false,
-    contentType: 'application/json',
-    dataType: 'json'
-  });
-*/
-  
-  display_account();
-
 });
 
-
-$("#sign-in").click(function() {
-  var mnemonic = $("#mnemonics").val();
-  console.log(mnemonic);
-  var seed = BIP39.mnemonic2seed(mnemonic);
-
-
-  var hkey = HierarchicalKey.seed(seed);
-  child = hkey.derive("m/0'");
-
-  var xpub = child.extendedPublicKeyString();
-  console.log(xpub);
+$("#sign-in").submit(function(event) {
+  event.preventDefault();
+  $("#account-forms").fadeOut('fast', function () {
+    $("#loader").show();
+    var mnemonic = $("#mnemonics").val();
+    console.log(mnemonic);
+    var seed = BIP39.mnemonic2seed(mnemonic);
 
 
-  uuid = UUID.v5(xpub, "urn:37coins.com");
+    var hkey = HierarchicalKey.seed(seed);
+    child = hkey.derive("m/0'");
 
-  console.log(uuid);
+    var xpub = child.extendedPublicKeyString();
+    console.log(xpub);
 
-  var wallet = $.ajax({
-    type: "GET",
-    url: "http://chains-qa.37coins.io/keychains/" + uuid,
-    async: false,
-    contentType: 'application/json',
-    dataType: 'json'
+
+    uuid = UUID.v5(xpub, "urn:37coins.com");
+
+    console.log(uuid);
+
+    var wallet = $.ajax({
+      type: "GET",
+      url: "http://chains-qa.37coins.io/keychains/" + uuid,
+      async: false,
+      contentType: 'application/json',
+      dataType: 'json'
+    });
+    
+    console.log(wallet);
+    display_account();
   });
-  
-  console.log(wallet);
-  display_account();
+});
 
+$("#sign-out").click(function(event) {
+  $('body').fadeOut('fast', function () {
+    window.location.reload(true);
+  });
 });
 
 });
